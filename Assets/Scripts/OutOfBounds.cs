9c7aa96d3df9b092226e6bea6bdca655db4480be
bryant-ta <33585269+bryant-ta@users.noreply.github.com>
2023-11-02 17:59:09 -0700

    changed main scene background and ui art

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class OutOfBounds : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D other) {
-		if (other.TryGetComponent(out Guy guy) && guy.Haslanded) {
+		if (other.TryGetComponent(out Guy guy) && guy.Haslanded && !GameManager.Instance.LostGame) {
 			GameManager.Instance.LoseGame();
 		}
 	}
